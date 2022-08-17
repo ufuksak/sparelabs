18a4ee51375111ea01b80f994b918c2828cc42ab
ufuksakar <ufuksakar@gmail.com>
2022-08-17 21:00:20 +0300

    capmetro list vehicles added

--- src/products/dto/grid.ts
+++ src/products/dto/grid.ts
@@ -1,47 +1,32 @@
-import { Expose } from 'class-transformer'
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
 
 export class Grid {
-    @Expose()
+
     @IsString()
     @IsNotEmpty()
     GRID_NAME: string
 
-    @Expose()
     @IsString()
     @IsNotEmpty()
     USER_FUNCTION_NAME: string
 
-    @Expose()
     @IsNumber()
     @IsNotEmpty()
     GRID_ID: number
 
-    @Expose()
     @IsNumber()
     @IsNotEmpty()
     NUMBER_OF_ROWS_FIRST_RETURNED: number
 
-    @Expose()
     @IsNumber()
     @IsNotEmpty()
     CURSOR_POSITION: number
 
-    @Expose()
     CURRENT_TAB_NAME: string
-
-    @Expose()
     TAB_NAME: string
-
-    @Expose()
     RESULT_IN_SAXORDER: string
-
-    @Expose()
     TERSERESPONSE: string
-
-    @Expose()
     LOCALIZE_RESULT: string
-    @Expose()
     CUSTOM_FIELDS_CLASS_FILTER: string
 }
 
@@ -72,20 +57,15 @@ export class LovParameter {
 }
 
 export class GridModel {
-
-    @Expose()
     @IsNotEmpty()
     GRID: Grid
 
-    @Expose()
     @IsNotEmpty()
     GRID_TYPE: GridType
 
-    @Expose()
     @IsNotEmpty()
     DATASPY: Dataspy
 
-    @Expose()
     @IsNotEmpty()
     LOV: Lov
 
